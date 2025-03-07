40c83aaf0f5395aac1e184d2245910d6d31ab5da
dax <daxnavarrete@gmail.com>
2025-03-06 23:18:33 -0500

    feat(config): add config for .env and security

---
+++ src/fastapi_auth/core/security.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from typing import Optional
+import jwt
+from .config import settings
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    print(settings.SECRET_KEY)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
